81e787579d02c5eaf683d8e30bde2afce4d708c1
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-18 14:17:44 +0530

    correcting backend logic

--- my-turborepo/apps/leaderboard_backend/Server.js
+++ my-turborepo/apps/leaderboard_backend/Server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // React frontend ko allow karne ke liye
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -31,15 +31,13 @@ app.get("/", (req, res) => {
 
 const LEADERBOARD_KEY = 'global_arena_rank';
 
-// --- MAIN FUNCTION: Redis se Top Players nikalna ---
+
 const broadcastLeaderboard = async () => {
   try {
-    // ZREVRANGE: Highest score se lowest score tak fetch karta hai
-    // WITHSCORES: Naam ke sath uska score bhi layega
+ 
     const topPlayers = await redis.zrevrange(LEADERBOARD_KEY, 0, 99, 'WITHSCORES');
     
-    // Redis data ajeeb array me deta hai: ["Ninja", "150", "ProDev", "100"]
-    // Hum isko proper Object [{name: "Ninja", score: 150}] me convert kar rahe hain
+    
     const formattedLeaderboard = [];
     for (let i = 0; i < topPlayers.length; i += 2) {
       formattedLeaderboard.push({
@@ -48,33 +46,30 @@ const broadcastLeaderboard = async () => {
       });
     }
 
-    // Sabhi connected browsers ko fresh leaderboard bhej do!
+
     io.emit('update_leaderboard', formattedLeaderboard);
   } catch (error) {
     console.error("Leaderboard fetch error:", error);
   }
 };
 
-// --- SOCKET.IO EVENTS ---
 io.on('connection', (socket) => {
   console.log(`/-! Player Connected: ${socket.id}`);
 
-  // Jaise hi koi naya tab khole, usko turant purana leaderboard dikha do
   broadcastLeaderboard();
 
-  // 1. Jab naya bacha apna naam daale
+
   socket.on('player_joined', async (data) => {
-    // Redis me user add karo (agar pehle se nahi hai). Score update.
+    
     await redis.zadd(LEADERBOARD_KEY, data.score, data.name);
-    broadcastLeaderboard(); // Update sabko bhejo
+    broadcastLeaderboard(); 
   });
 
-  // 2. Jab code run ho aur challenge complete ho (Scores badhein)
   socket.on('submit_score', async (data) => {
     console.log(`Score Aaya -> ${data.name}: ${data.score} points`);
-    // Redis ZADD apne aap purane score ko naye wale se replace/update kar dega aur sort kar dega
+    
     await redis.zadd(LEADERBOARD_KEY, data.score, data.name);
-    broadcastLeaderboard(); // Update sabko bhejo
+    broadcastLeaderboard(); 
   });
 
   socket.on('disconnect', () => {
