a2cccce1163b211570c64bc83e78553e61e97c5e
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-18 13:16:54 +0530

    Complete express setup noe work on redish

--- my-turborepo/apps/leaderboard_backend/Server.js
+++ my-turborepo/apps/leaderboard_backend/Server.js
@@ -4,3 +4,17 @@ import { Redis } from "ioredis";
 
 
 const app = express();
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Leaderboard API is running');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`/-! Backend running on http://localhost:${PORT}`);
+});
